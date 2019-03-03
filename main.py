e55d6290d19dd467ffa7321e8e6c4bd9622081ed
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2019-03-03 11:06:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import warnings
 warnings.filterwarnings('ignore')
-import cv2, os
+import cv2, os, math
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -18,7 +18,7 @@ def process(image):
     _, contours, hierarchy = cv2.findContours(thresh1.copy(),cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
     cnt = max(contours, key = lambda x: cv2.contourArea(x))
     area_of_contour = cv2.contourArea(cnt)
-    x,y,w,h = cv2.boundingRect(cnt)
+    x, y, w, h = cv2.boundingRect(cnt)
     roi_1 = roi.copy()
     roi_2 = roi.copy()
     cv2.rectangle(roi_1, (x, y), (x + w, y + h), (0, 0, 255), 1)
@@ -26,16 +26,46 @@ def process(image):
     drawing = np.zeros(roi_2.shape, np.uint8)
     cv2.drawContours(drawing, [cnt], 0, (0, 255, 0), 0)
     cv2.drawContours(drawing, [hull], 0, (0, 255, 255), 0)
-    return roi_1, drawing
+    hull  = cv2.convexHull(cnt, returnPoints = False)
+    defects = cv2.convexityDefects(cnt, hull)
+    cv2.drawContours(thresh1, contours, -1, (0,255,0), 3)
+    count_defects = 0
+    for i in range(defects.shape[0]):
+        s, e, f, d = defects[i, 0]
+        start = tuple(cnt[s][0])
+        end = tuple(cnt[e][0])
+        far = tuple(cnt[f][0])
+        a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
+        b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
+        c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
+        angle = math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c)) * 60
+        cv2.circle(crop_img, far, 4, [0, 0, 255], -1)
+        if angle <= 90:
+            count_defects += 1
+        cv2.line(crop_img, start, end, [0, 255, 0], 3)
+
+    return roi_1, drawing, thresh1, crop_img, count_defects
 
 def main():
     cap = cv2.VideoCapture(0)
     while cap.isOpened():
         try:
             ret, frame = cap.read()
-            res, drawing = process(frame)
-            cv2.imshow('result', res)
-            cv2.imshow('drawing', drawing)
+            res, drawing, thresh1, crop_img, count_defects = process(frame)
+            fingers = count_defects + 1
+            cv2.putText(
+                frame,
+                'Fingers: ' + str(fingers),
+                (10, 500),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                4, (255, 255, 255),
+                2, cv2.LINE_AA
+            )
+            cv2.imshow('Frame', frame)
+            # cv2.imshow('result', res)
+            # cv2.imshow('drawing', drawing)
+            # cv2.imshow('thresh', thresh1)
+            # cv2.imshow('crop', crop_img)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         except:
